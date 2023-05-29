e90057a2cb44a6c4d43069829ac7a692644d1eeb
vladorehov <113713131+vladorehov@users.noreply.github.com>
2023-05-29 22:29:18 +0300

    Program.cs
    ...

--- Program.cs
+++ Program.cs
@@ -6,73 +6,156 @@ namespace orehov1
     {
         static void Main(string[] args)
         {
-            string s1 = "hello";
-            string s2 = "world";
-            Console.WriteLine(s1 + s2);
+           Console.WriteLine("Выберите тему и задание, варианты: 11 12 13 14 15 21 22 23 24 ");
+            int vibor = Convert.ToInt32(Console.ReadLine());
+            if (vibor == 11)
+            {
+                int chis = Convert.ToInt32(Console.ReadLine());
+                if (chis < 0)
+                {
+                    Console.WriteLine(chis - 5);
+                }
+                if (chis > 0)
+                {
+                    Console.WriteLine(chis + 5);
+                }
+            }
+            if (vibor == 12)
+            {
+                int chis1 = Convert.ToInt32(Console.ReadLine());
+                int chis2 = Convert.ToInt32(Console.ReadLine());
+                int chis3 = Convert.ToInt32(Console.ReadLine());
+                if (chis1 > chis2 && chis1 > chis3)
+                {
+                    Console.WriteLine(chis1);
+                }
+                if (chis2 > chis1 && chis2 > chis3)
+                {
+                    Console.WriteLine(chis2);
+                }
+                if (chis3 > chis2 && chis3 > chis1)
+                {
+                    Console.WriteLine(chis3);
+                }
+            }
+            if (vibor == 13)
+            {
+                int a = 0;
+                int b = 0;
+                int c = 0;
 
-            Console.ReadLine();
+                int chis11 = Convert.ToInt32(Console.ReadLine());
+                int chis21 = Convert.ToInt32(Console.ReadLine());
+                int chis31 = Convert.ToInt32(Console.ReadLine());
+                if (chis11 > 0)
+                {
+                    a = chis11;
+                }
+                if (chis21 > 0)
+                {
+                    b = chis21;
+                }
+                if (chis31 > 0)
+                {
+                    c = chis31;
+                }
+                int abc = a + b + c;
+                Console.WriteLine(abc);
+            }
+            if (vibor == 14)
+            {
+                Console.WriteLine("введите  х");
+                int x = Convert.ToInt32(Console.ReadLine());
+                if (x > 10)
+                {
+                    Console.WriteLine(x * 2);
+                }
+            }
+            if (vibor == 15)
+            {
+                Console.WriteLine("sdfjhsbdhfbsdhf");
+                int den = Convert.ToInt32(Console.ReadLine());
+                switch (den)
+                {
+                    case 1: Console.WriteLine("понедельник"); break;
+                    case 2: Console.WriteLine("вторник"); break;
+                    case 3: Console.WriteLine("среда"); break;
+                    case 4: Console.WriteLine("четверг"); break;
+                    case 5: Console.WriteLine("пятница"); break;
+                    case 6: Console.WriteLine("суббота"); break;
+                    case 7: Console.WriteLine("воскресенье"); break;
+                }
+            }
+            if (vibor == 21)
+            {
+                int number = 200;
+                while (number % 17 != 0)
+                {
+                    number++;
+                }
+                Console.WriteLine(number);
+            }
+            if (vibor == 22)
+            {
+                bool den20 = false;
+                bool prob100 = false;
+                int denn = 1;
+                double vse = 10;
+                double beg = 10;
+                while (beg < 100)
+                {
 
-            Console.WriteLine("Введите первое число");
-            int p1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите второе число");
-            int p2 = Convert.ToInt32(Console.ReadLine());
-            int r = 0;
-            Console.WriteLine("Введите знак");
-            string znak = Console.ReadLine();
-            if (znak == "+")
-            {
-                r = p1 + p2;
+                    denn++;
+                    beg += beg / 20;
+                    vse += beg;
+                    if (beg >= 20 && den20 == false)
+                    {
+                        Console.WriteLine("День, когда он пробежал 20км за день: " + denn);
+                        den20 = true;
+                    }
+                    if (vse >= 100 && prob100 == false)
+                    {
+                        Console.WriteLine("День, когда он пробежал в общем 100км : " + denn);
+                        prob100 = true;
+                    }
+                }
             }
-            if (znak == "-")
+            if (vibor == 23)
             {
-                r = p1 - p2;
+                int f = 0;
+                int ff = 1;
+                Console.WriteLine("Факториал нада? ");
+                int fact = Convert.ToInt32(Console.ReadLine());
+                for (f = 1; f <= fact; f++)
+                {
+                    ff = ff * f;
+                }
+                Console.WriteLine(ff);
             }
-            if (znak == "*")
+            if (vibor == 24)
             {
-                r = p1 * p2;
+                Console.WriteLine("Вводи ");
+                int nub = Convert.ToInt32(Console.ReadLine());
+                int del = 1;
+                while (nub < 1000)
+                {
+                    del++;
+                    int ost = nub % del;
+                    if (ost == 0)
+                    {
+                        Console.WriteLine(del);
+                        break;
+                    }
+
+                }
             }
-            if (znak == "/")
-            {
-                r = p1 / p2;
-            }
-            Console.WriteLine(r);
+
+
+
+
+
 
             Console.ReadKey();
-
-            Console.WriteLine("Введите Имя");
-            string name = Console.ReadLine();
-            Console.WriteLine("Введите фамилию");
-            string fam = Console.ReadLine();
-            Console.WriteLine("Введите отчество");
-            string otch = Console.ReadLine();
-            Console.Write(name+" "+fam+ " " + otch);
-
-            Console.ReadKey();
-
-            int hp_p = 100;
-            int hp_m = 65;
-            int def_p = 15;
-            int def_m = 10;
-            int atk_p = 30;
-            int atk_m = 20;
-            
-                
-                Console.WriteLine("Ваше хп " + hp_p);
-                Console.WriteLine("хп монстра " + hp_m);
-                    
-                Console.WriteLine("Время удара (нажмите любую кнопку кроме Power)");
-                Console.ReadKey();
-                int atk2p = atk_p / def_m;
-                hp_m = hp_m - atk2p;
-                int atk2m = atk_m / def_p;
-                hp_p = hp_p - atk2m;
-                Console.WriteLine("Ваше хп " +hp_p);
-                Console.WriteLine("хп монстра "+hp_m);
-                Console.WriteLine("вы нанесли монстру "+ atk2p);
-                Console.WriteLine("он нанес вам "+ atk2m);
-
-                
-            
         }
     }
 }
